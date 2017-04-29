654683227c2867a45a62b57d2ab910e21ff51655
noanachmias <nachmias5@walla.co.il>
2017-04-29 19:34:26 +0300

    good

--- app/src/main/java/com/example/shirl1/mykitchenland/AddRecActivity.java
+++ app/src/main/java/com/example/shirl1/mykitchenland/AddRecActivity.java
@@ -1,6 +1,7 @@
 package com.example.shirl1.mykitchenland;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +9,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.util.Log;
 import java.util.ArrayList;
@@ -60,6 +62,8 @@ public class AddRecActivity extends AppCompatActivity {
     EditText re_instructions;
     EditText re_ingredient;
     EditText re_amount;
+    LinearLayout lay_in;
+    LinearLayout lay_am;
 
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -67,10 +71,13 @@ public class AddRecActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_rec);
         db = new DBHandler(this);
 
-        re_name = (EditText) findViewById(R.id.input_name);
+        //re_name = (EditText) findViewById(R.id.input_name);
         re_instructions = (EditText) findViewById(R.id.input_instructions);
-        re_ingredient = (EditText) findViewById(R.id.input_ingre);
+        //re_ingredient = (EditText) findViewById(R.id.input_ingre);
         re_amount = (EditText) findViewById(R.id.input_amount);
+        lay_in = (LinearLayout) findViewById(R.id.lay_ingredient);
+        lay_am = (LinearLayout) findViewById(R.id.lay_amount);
+
 
         //db.addRecipe(new Recipes("RECIP1"));
         //db.addRecipe(new Recipes("RECIP2"));
@@ -79,6 +86,19 @@ public class AddRecActivity extends AppCompatActivity {
         TextView t1 = (TextView) findViewById(R.id.textView1);
         t1.isClickable();*/
 
+    }
+
+    //HOW TO ADD ALL ROWS EACH PRESS TO TABLE
+    public void btn_add_ingre_on_click(View v){
+
+        EditText e_in = new EditText(this);
+        EditText e_am = new EditText(this);
+
+        String in1 = e_in.getText().toString();
+        String in2 = e_am.getText().toString();
+
+        lay_in.addView(e_in);
+        lay_am.addView(e_am);
     }
 
 
